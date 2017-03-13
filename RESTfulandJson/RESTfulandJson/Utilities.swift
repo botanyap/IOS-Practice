2504743c6c28510827d8b1b290fa05be118a28c0
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2017-03-13 10:03:31 +0700

    update RESTfulandJson

--- RESTfulandJson/RESTfulandJson/Utilities.swift
+++ RESTfulandJson/RESTfulandJson/Utilities.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class Utilities {
+    func getStorage() -> UserDefaults {
+        return UserDefaults.standard
+    }
+    
+    func setSkinType (value: String) {
+        let defaults = getStorage()
+        defaults.setValue(value, forKey:defaultsKeys.skinType)
+        defaults.synchronize()
+    }
+    
+    func getSkinType() -> String {
+        let defaults = getStorage()
+        if let result = defaults.string(forKey: defaultsKeys.skinType){
+            return result
+        }
+        return SkinType().type1
+        
+    }
+    
+}
+
+struct SkinType {
+    let type1 = "Type 1 - pale / Light"
+    let type2 = "Type 2 - white / Fair"
+    let type3 = "Type 3 - Mrdium"
+    let type4 = "Type 4 - Olive Brown"
+    let type5 = "Type 5 - Dark Brown"
+    let type6 = "Type 6 - Very Dark / Black"
+    
+}
+
+struct defaultsKeys {
+    static let skinType = "skinType"
+}
+    
--- RESTfulandJson/RESTfulandJson/ViewController.swift
+++ RESTfulandJson/RESTfulandJson/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var skinTypelbl: UILabel!
+    var skintype = SkinType().type1 {
+        didSet {
+            skinTypelbl.text = self.skintype
+            Utilities().setSkinType(value: skintype)
+        }
+    }
 
+    let locationManager = CLLocationManager()
+    var coords = CLLocationCoordinate2D(latitude: 40,longitude: 40)
+    
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        skintype = Utilities().getSkinType()
+        skinTypelbl.text = "Skin : " + skintype
+        
     }
+    
+    
+    @IBAction func changeSkinClicked(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Skin Type", message: "Please Choose Skin Type", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: SkinType().type1, style: .default, handler: { (action) in
+            self.skintype = SkinType().type1
+        }))
+        alert.addAction(UIAlertAction(title: SkinType().type2, style: .default, handler: { (action) in
+            self.skintype = SkinType().type2
+        }))
+
+        alert.addAction(UIAlertAction(title: SkinType().type3, style: .default, handler: { (action) in
+            self.skintype = SkinType().type3
+        }))
+
+        alert.addAction(UIAlertAction(title: SkinType().type4, style: .default, handler: { (action) in
+            self.skintype = SkinType().type4
+        }))
+
+        alert.addAction(UIAlertAction(title: SkinType().type5, style: .default, handler: { (action) in
+            self.skintype = SkinType().type5
+        }))
+
+        alert.addAction(UIAlertAction(title: SkinType().type6, style: .default, handler: { (action) in
+            self.skintype = SkinType().type6
+        }))
+        self.present(alert, animated: true, completion: nil)
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Location changed")
+        
+        if status == .authorizedWhenInUse {
+            getLocation()
+        }else if status == .denied {
+            let alert = UIAlertController(title: "Error", message:"Go to setting and allow Location for this app!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func getLocation() {
+        if  let loc = locationManager.location?.coordinate {
+            coords = loc
+        }
+        
+    }
 }
 
